e0230daf03a5d38662315b67dbfdcd96bf88b80e
lucianosants <lucianosantos.developer@gmail.com>
2024-09-07 14:49:57 -0300

    feat: implement global authentication

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,8 +5,11 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+
+import { IS_PUBLIC_KEY } from './decorators/is-public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -15,12 +18,22 @@ export class AuthGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
+    private readonly reflector: Reflector,
   ) {
     this.jwtSecret = this.configService.get<string>('JWT_SECRET');
     this.jwtSecret = process.env.JWT_SECRET;
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
 
     const token = this.extractTokenFromHeader(request);
